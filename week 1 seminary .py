fcdadf2e0d274f54b34a9b6385bbb8e358dc1444
jokerlek13 <iulianleca13@gmail.com>
2023-11-04 10:00:47 +0000

    hi

---
+++ week 1 seminary .py
@@ -0,0 +1,30 @@
+"""print('Hello world!')
+print('What school do you study?')
+school = input()
+print('I study in ' + school)
+print('what is your name?')
+name = input()
+print('hi ' + name)
+print('Where do you live?')
+address = input()
+print('Your address is ' + address)"""
+
+"""
+print('Give me one number:')
+number1 = int(input())
+print('Give me another number:')
+number2 = int(input())
+print('The total is ' + str(number1 + number2))
+
+name = 'Iulian'
+print(type(name))
+
+age = 50
+print(type(age))
+
+salary = 54.45
+print(type(salary))
+isStudent = True
+print(type(isStudent))
+
+"""
